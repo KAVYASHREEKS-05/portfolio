c602c0a4160b060b250457e7ba1b5908a2524367
KAVYASHREEKS-05 <140942756+KAVYASHREEKS-05@users.noreply.github.com>
2024-07-25 12:24:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+const nav=document.querySelector('.nav')
+const hamburger=document.querySelector('.hamburger')
+const downloadResume=document.querySelector('.resume')
+const readMoreButton=document.querySelector(".read-more")
+const extraInf=document.querySelector(".extra-inf")
+const InternCertificate=document.querySelector('.intern')
+const HTMLCSSCertificate=document.querySelector('.html-css')
+const JavascriptCertificate=document.querySelector('.javascript')
+hamburger.addEventListener('click',()=>{
+    nav.classList.toggle('open')
+    hamburger.classList.toggle('hamburger-open')
+})
+downloadResume.addEventListener('click',()=>{
+    const a=document.createElement('a')
+    a.href="my-resume.pdf";
+    a.document="my-resume.pdf";
+    a.target = '_blank';
+    document.body.appendChild(a)
+    a.click();
+    document.body.removeChild(a);
+})
+readMoreButton.onclick = () => {
+    if (extraInf.classList.contains('hidden')) {
+      extraInf.classList.remove('hidden');
+      readMoreButton.innerText = 'Hide';
+    } else {
+      extraInf.classList.add('hidden');
+      readMoreButton.innerText = 'Read More';
+    }
+  };
+  InternCertificate.addEventListener('click',()=>{
+    const a=document.createElement('a')
+    a.href="certificates/internship.pdf";
+    a.document="certificates/internship.pdf";
+    a.target = '_blank';
+    document.body.appendChild(a)
+    a.click();
+    document.body.removeChild(a);
+})
+  HTMLCSSCertificate.addEventListener('click',()=>{
+    const a=document.createElement('a')
+    a.href="certificates/html and css.pdf";
+    a.document="certificates/html and css.pdf";
+    a.target = '_blank';
+    document.body.appendChild(a)
+    a.click();
+    document.body.removeChild(a);
+})
+  JavascriptCertificate.addEventListener('click',()=>{
+    const a=document.createElement('a')
+    a.href="certificates/javascript certificate.pdf";
+    a.document="certificates/javascript certificate.pdf";
+    a.target = '_blank';
+    document.body.appendChild(a)
+    a.click();
+    document.body.removeChild(a);
+})
+
+window.addEventListener('scroll',reveal)
+
+function reveal(){
+  var reveals=document.querySelectorAll('.reveal')
+  for(var i=0;i<reveals.length;i++){
+    var windowHeight=window.innerHeight
+    var revealTop=reveals[i].getBoundingClientRect().top;
+    var revealPoint=150;
+
+    if(revealTop<windowHeight-revealPoint){
+      reveals[i].classList.add('active')
+    }
+    else{
+      reveals[i].classList.remove('active')
+    }
+  }
+}
