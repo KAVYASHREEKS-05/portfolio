ef350d7067aeb97d24f5853507f622ffb2f908c4
Kavyashree K S <kavyaks793@gmail.com>
2024-08-15 20:23:31 +0530

    Changes has been done

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const downloadResume=document.querySelector('.resume')
 const readMoreButton=document.querySelector(".read-more")
 const extraInf=document.querySelector(".extra-inf")
 const InternCertificate=document.querySelector('.intern')
-const HTMLCSSCertificate=document.querySelector('.html-css')
-const JavascriptCertificate=document.querySelector('.javascript')
+const MetaCertificate=document.querySelector('.meta')
+
 hamburger.addEventListener('click',()=>{
     nav.classList.toggle('open')
     hamburger.classList.toggle('hamburger-open')
 })
 downloadResume.addEventListener('click',()=>{
     const a=document.createElement('a')
-    a.href="my-resume.pdf";
-    a.document="my-resume.pdf";
+    a.href="My-resume.pdf";
+    a.document="My-resume.pdf";
     a.target = '_blank';
     document.body.appendChild(a)
     a.click();
@@ -37,24 +37,16 @@ readMoreButton.onclick = () => {
     a.click();
     document.body.removeChild(a);
 })
-  HTMLCSSCertificate.addEventListener('click',()=>{
+  MetaCertificate.addEventListener('click',()=>{
     const a=document.createElement('a')
-    a.href="certificates/html and css.pdf";
-    a.document="certificates/html and css.pdf";
+    a.href="certificates/meta-certificate.pdf";
+    a.document="certificates/meta-certificate.pdf";
     a.target = '_blank';
     document.body.appendChild(a)
     a.click();
     document.body.removeChild(a);
 })
-  JavascriptCertificate.addEventListener('click',()=>{
-    const a=document.createElement('a')
-    a.href="certificates/javascript certificate.pdf";
-    a.document="certificates/javascript certificate.pdf";
-    a.target = '_blank';
-    document.body.appendChild(a)
-    a.click();
-    document.body.removeChild(a);
-})
+
 
 window.addEventListener('scroll',reveal)
 
@@ -73,3 +65,41 @@ function reveal(){
     }
   }
 }
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
